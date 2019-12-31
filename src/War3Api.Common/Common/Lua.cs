65f914a5ac0c02b3e8c36463911f737eab1c3991
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-12-31 11:53:39 +0100

    Disable codestyle warnings.

--- src/War3Api.Common/Common/Lua.cs
+++ src/War3Api.Common/Common/Lua.cs
@@ -7,15 +7,15 @@
 
 /* Not affiliated or endorsed by Blizzard Entertainment */
 
-#pragma warning disable IDE0052, IDE1006, CS0626, SA1601
-
-using War3Net.CodeAnalysis.Common;
+#pragma warning disable IDE0052, IDE1006, CS0626
+#pragma warning disable CA1034, CA1707, CA1716, CA2211,
+#pragma warning disable SA1201, SA1202, SA1203, SA1300, SA1303, SA1307, SA1310, SA1311, SA1313, SA1401, SA1407, SA1514, SA1516, SA1600, SA1601, SA1604, SA1611, SA1615, SA1626
 
 namespace War3Api
 {
     public static partial class Common
     {
-        [NativeLuaMember]
+        /// @CSharpLua.Template = "FourCC({0})"
         public static extern int FourCC(string value);
     }
 }
