638503d5d8d9f9c9f03e560eb5e6fe7969c48ac3
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-04-19 13:00:14 +0200

    Constants.

---
+++ src/War3Api.Generator.Object/DataConstants.cs
@@ -0,0 +1,84 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="DataConstants.cs" company="Drake53">
+// Copyright (c) 2020 Drake53. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+namespace War3Api.Generator.Object
+{
+    internal static class DataConstants
+    {
+        // Base
+        internal const string BaseClassName = "BaseObject";
+
+        // Unit
+        internal const string UnitClassName = "Unit";
+        internal const string UnitTypeEnumName = "UnitType";
+        internal const string UnitTypeEnumParameterName = "baseUnitType";
+
+        internal const string UnitDataKeyColumn = "unitID";
+        internal const string UnitAbilityDataKeyColumn = "unitAbilID";
+        internal const string UnitBalanceDataKeyColumn = "unitBalanceID";
+        internal const string UnitUiDataKeyColumn = "unitUIID";
+        internal const string UnitWeaponDataKeyColumn = "unitWeaponID";
+
+        // Item
+        internal const string ItemClassName = "Item";
+        internal const string ItemTypeEnumName = "ItemType";
+        internal const string ItemTypeEnumParameterName = "baseItemType";
+
+        internal const string ItemDataKeyColumn = "itemID";
+
+        // Destructable
+        internal const string DestructableClassName = "Destructable";
+        internal const string DestructableTypeEnumName = "DestructableType";
+        internal const string DestructableTypeEnumParameterName = "baseDestructableType";
+
+        internal const string DestructableDataKeyColumn = "DestructableID";
+
+        // Doodad
+        internal const string DoodadClassName = "Doodad";
+        internal const string DoodadTypeEnumName = "DoodadType";
+        internal const string DoodadTypeEnumParameterName = "baseDoodadType";
+
+        internal const string DoodadDataKeyColumn = "doodID";
+
+        // Ability
+        internal const string AbilityClassName = "Ability";
+        internal const string AbilityTypeEnumName = "AbilityType";
+        internal const string AbilityTypeEnumParameterName = "baseAbilityType";
+
+        internal const string AbilityDataKeyColumn = "alias";
+
+        // Buff
+        internal const string BuffClassName = "Buff";
+        internal const string BuffTypeEnumName = "BuffType";
+        internal const string BuffTypeEnumParameterName = "baseBuffType";
+
+        internal const string BuffDataKeyColumn = "alias";
+
+        // Upgrade
+        internal const string UpgradeClassName = "Upgrade";
+        internal const string UpgradeTypeEnumName = "UpgradeType";
+        internal const string UpgradeTypeEnumParameterName = "baseUpgradeType";
+
+        internal const string UpgradeDataKeyColumn = "upgradeid";
+
+        // Metadata
+        internal const string MetaDataIdColumn = "ID";
+        internal const string MetaDataFieldColumn = "field";
+        internal const string MetaDataRepeatColumn = "repeat";
+        internal const string MetaDataDisplayNameColumn = "displayName";
+        internal const string MetaDataSortColumn = "sort";
+        internal const string MetaDataTypeColumn = "type";
+        internal const string MetaDataMinValColumn = "minVal";
+        internal const string MetaDataMaxValColumn = "maxVal";
+        internal const string MetaDataUseHeroColumn = "useHero";
+        internal const string MetaDataUseUnitColumn = "useUnit";
+        internal const string MetaDataUseBuildingColumn = "useBuilding";
+        internal const string MetaDataUseItemColumn = "useItem";
+        internal const string MetaDataUseCreepColumn = "useCreep";
+        internal const string MetaDataUseSpecificColumn = "useSpecific";
+    }
+}
---
+++ src/War3Api.Generator.Object/PathConstants.cs
@@ -0,0 +1,50 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="PathConstants.cs" company="Drake53">
+// Copyright (c) 2020 Drake53. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+namespace War3Api.Generator.Object
+{
+    internal static class PathConstants
+    {
+        // Base
+        internal const string WorldEditStringsPath = @"_locales\enus.w3mod\ui\worldeditstrings.txt";
+        internal const string EnumDataFilePath = @"ui\uniteditordata.txt";
+
+        // Unit
+        internal const string UnitDataPath = @"units\unitdata.slk";
+        internal const string UnitAbilityDataPath = @"units\unitabilities.slk";
+        internal const string UnitBalanceDataPath = @"units\unitbalance.slk";
+        internal const string UnitUiDataPath = @"units\unitui.slk";
+        internal const string UnitWeaponDataPath = @"units\unitweapons.slk";
+
+        // Item
+        internal const string ItemDataPath = @"units\itemdata.slk";
+
+        // Destructable
+        internal const string DestructableDataPath = @"units\destructabledata.slk";
+
+        // Doodad
+        internal const string DoodadDataPath = @"doodads\doodads.slk";
+
+        // Ability
+        internal const string AbilityDataPath = @"units\abilitydata.slk";
+
+        // Buff
+        internal const string BuffDataPath = @"units\abilitybuffdata.slk";
+
+        // Upgrade
+        internal const string UpgradeDataPath = @"units\upgradedata.slk";
+
+        // Metadata
+        internal const string UnitMetaDataPath = @"units\unitmetadata.slk";
+        internal const string ItemMetaDataPath = UnitMetaDataPath;
+        internal const string DestructableMetaDataPath = @"units\destructablemetadata.slk";
+        internal const string DoodadMetaDataPath = @"doodads\doodadmetadata.slk";
+        internal const string AbilityMetaDataPath = @"units\abilitymetadata.slk";
+        internal const string BuffMetaDataPath = @"units\abilitybuffmetadata.slk";
+        internal const string UpgradeMetaDataPath = @"units\upgrademetadata.slk";
+    }
+}
