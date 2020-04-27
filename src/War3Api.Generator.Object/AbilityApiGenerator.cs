71d59e3c2cc341cb1b890e9161cbf0763e462e23
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-04-27 10:39:04 +0200

    Fix invalid AbilityType enum member.

--- src/War3Api.Generator.Object/AbilityApiGenerator.cs
+++ src/War3Api.Generator.Object/AbilityApiGenerator.cs
@@ -85,7 +85,7 @@ namespace War3Api.Generator.Object
                     var abilityTypeEnumMemberModel = new EnumMemberModel();
 
                     var name = ObjectApiGenerator.Localize((string)abilityType[commentColumn]);
-                    abilityTypeEnumMemberModel.Name = name.Dehumanize();
+                    abilityTypeEnumMemberModel.Name = name.Dehumanize().TrimStart('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
                     abilityTypeEnumMemberModel.DisplayName = name;
                     abilityTypeEnumMemberModel.Value = ((string)abilityType[abilityIdColumn]).FromRawcode();
 
