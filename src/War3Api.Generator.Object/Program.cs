a248ff8dcdf5c099f661fc4e01017bf1e259a7f1
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-05-08 19:03:04 +0200

    Program.cs

---
+++ src/War3Api.Generator.Object/Program.cs
@@ -0,0 +1,41 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Drake53">
+// Copyright (c) 2020 Drake53. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.IO;
+
+namespace War3Api.Generator.Object
+{
+    internal static class Program
+    {
+        private const string Version = "1.31.1.12173";
+        private const string InputFolder = @"..\..\..\API";
+        private const string OutputFolder = @"..\..\..\..\War3Api.Object\Generated";
+
+        private const string InputAbsolute = null;
+        private const string OutputAbsolute = null;
+
+        private static void Main(string[] args)
+        {
+            var inputFolder = InputAbsolute ?? Path.Combine(InputFolder, Version);
+            var outputFolder = OutputAbsolute ?? Path.Combine(OutputFolder, Version);
+
+            ObjectApiGenerator.InitializeGenerator(inputFolder, outputFolder);
+            UnitApiGenerator.Generate(inputFolder);
+            ItemApiGenerator.Generate(inputFolder);
+            DestructableApiGenerator.Generate(inputFolder);
+            DoodadApiGenerator.Generate(inputFolder);
+            AbilityApiGenerator.Generate(inputFolder);
+            BuffApiGenerator.Generate(inputFolder);
+            UpgradeApiGenerator.Generate(inputFolder);
+
+            foreach (var classFile in Directory.EnumerateFiles(@"..\..\..\Classes"))
+            {
+                File.Copy(classFile, Path.Combine(outputFolder, new FileInfo(classFile).Name), true);
+            }
+        }
+    }
+}
